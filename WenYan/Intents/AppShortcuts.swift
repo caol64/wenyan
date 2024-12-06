a0914147a6e48bae0cbc5224edaf2696db77aeeb
Lei <caol64@gmail.com>
2024-12-06 15:39:49 +0800

    updates

---
+++ WenYan/Intents/AppShortcuts.swift
@@ -0,0 +1,20 @@
+//
+//  AppShortcuts.swift
+//  WenYan
+//
+//  Created by Lei Cao on 2024/12/6.
+//
+
+//import AppIntents
+//
+//@available(macOS 13.0, *)
+//struct AppShortcuts: AppShortcutsProvider {
+//
+//    static var appShortcuts: [AppShortcut] {
+//        AppShortcut(
+//            intent: StartAppIntent(),
+//            phrases: ["Start a \(.applicationName)"]
+//        )
+//    }
+//
+//}
---
+++ WenYan/Intents/StartAppIntent.swift
@@ -0,0 +1,19 @@
+//
+//  StartAppIntent.swift
+//  WenYan
+//
+//  Created by Lei Cao on 2024/12/6.
+//
+
+//import AppIntents
+//
+//@available(macOS 13.0, *)
+//struct StartAppIntent: AppIntent {
+//    static let title: LocalizedStringResource = "Start Meditation Session"
+//
+//    @MainActor
+//    func perform() async throws -> some IntentResult {
+//        print("perform")
+//        return .result()
+//    }
+//}
