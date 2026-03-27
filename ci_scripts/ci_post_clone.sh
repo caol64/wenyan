83b8cdf5e28b902b5dadae5544b6128ea615fba9
Lei <caol64@gmail.com>
2026-03-27 10:53:42 +0800

    fix: correct directory path for CI workspace in post-clone script

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -8,10 +8,10 @@ brew install node
 npm install -g pnpm
 
 # $CI_WORKSPACE 是 Xcode Cloud 提供的环境变量，指向项目的根目录。
-cd $CI_WORKSPACE
+cd "$CI_WORKSPACE/repository"
 
 pnpm ui:init
 
-cd $CI_WORKSPACE
+cd "$CI_WORKSPACE/repository"
 pnpm web:install
 pnpm web:build
